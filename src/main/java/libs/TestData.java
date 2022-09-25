d3ce5b48a0cf143f7686eeafac0bf852f255f7e4
Ndividual17 <ndividualkim17@gmail.com>
2022-09-25 04:29:55 +0300

    Create LoginTestWithPageObject

---
+++ src/main/java/libs/TestData.java
@@ -0,0 +1,9 @@
+package libs;
+
+/**
+ * A class where we will store the test data where it will be used in many places
+ */
+public class TestData {
+    public final static String VALID_LOGIN = "ndividualkim17+0@gmail.com";
+    public final static String VALID_PASSWORD = "jedi0000";
+}
---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import pages.elements.HeaderElement;
+
+public class HomePage extends ParentPage {
+    private HeaderElement headerElement = new HeaderElement(webDriver);
+
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    @Override
+    String getRelativeUrl() {
+        return "/";
+    }
+
+    public HomePage checkIsRedirectToHomePage() {
+        //TODO checkURL
+        Assert.assertTrue("HomePage is not loaded", getHeaderElement().isButtonLogoutDisplayed());
+        return this;
+    }
+
+    public HomePage openHomePage() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.openLoginPage();
+        if (!headerElement.isButtonLogoutDisplayed()) {
+            loginPage.loginWithValidCredWithOutOpenPage();
+        }
+        checkIsRedirectToHomePage();
+        return this;
+    }
+
+    public HeaderElement getHeaderElement() {
+        return headerElement;
+    }
+}
